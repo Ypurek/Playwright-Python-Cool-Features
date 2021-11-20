40008a4091f91a6bc63168fe3cf1dd3f1e1210ba
Ypurek <oleksii.ostapov@gmail.com>
2021-11-20 11:45:49 +0200

    updated example for js evaluation

--- test_evaluate.py
+++ test_evaluate.py
@@ -1,4 +1,9 @@
 # read more https://playwright.dev/python/docs/api/class-page#page-evaluate
 def test_js(alice):
-    height = alice.evaluate("window.innerHeight")
-    assert height == 720
+    result = alice.evaluate('''
+    var aaa = function(x, y){
+        return x + y;
+    }
+    aaa(2, 3);
+    ''')
+    assert result == 5
