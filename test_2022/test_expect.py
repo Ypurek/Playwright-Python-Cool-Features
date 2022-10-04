00ca3ceb9e3e7ad728d92d23e9d8dedc7800862e
Ypurek <oleksii.ostapov@infopulse.com>
2022-10-04 10:21:24 +0300

    added asserts alternatives for expect tests

--- test_2022/test_expect.py
+++ test_2022/test_expect.py
@@ -12,25 +12,37 @@ def alice_tc(alice):
 def test_expect_basic(alice_tc):
     status = alice_tc.locator('.testStatus_1')
 
-    expect(status).to_have_text('PASS')
+    # assert 'PASS' in status.text_content()
+    expect(status).to_have_text('PASS', timeout=5000)
+
+    # assert status.is_visible()
     expect(status).to_be_visible()
+
+    # assert not status.is_hidden()
     expect(status).not_to_be_hidden()
 
+    # assert '123456' not in status.get_attribute('class')
     expect(status).not_to_have_class('123456')
 
-    # expected full class tag
+    # assert 'testStatus_1 PASS' == status.get_attribute('class')
     expect(status).to_have_class('testStatus_1 PASS')
 
 
 def test_expect_details(alice_tc):
     pass_btn = alice_tc.locator('.passBtn.pass_1')
     # name and value are mandatory
+
+    # assert pass_btn.get_attribute('onclick') == "setStatus(1, 'PASS')"
     expect(pass_btn).to_have_attribute(name='onclick', value="setStatus(1, 'PASS')")
+
+    # no way to get css from locator ¯\_(ツ)_/¯
     expect(pass_btn).to_have_css(name='margin', value='0px 5px 0px 0px')
 
 
 def test_expect_count(alice_tc):
     menu_items = alice_tc.locator('.menuInner li')
+
+    # assert menu_items.count() == 5
     expect(menu_items).to_have_count(5)
 
 
@@ -40,5 +52,6 @@ def test_input(alice):
     test_name = alice.locator('#id_name')
     test_name.type(text)
 
-    # check input value without overhead
+    # assert test_name.input_value() == text
+    # check input value without overhead :)
     expect(test_name).to_have_value(text)
